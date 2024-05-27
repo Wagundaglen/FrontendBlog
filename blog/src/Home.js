f32a2d33e1c90f2418a1abdb934c681f4af6b90e
Glen Wagunda <glenwagunda@gmail.com>
2024-05-27 16:27:34 +0300

    first commit

---
+++ blog/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Discover Wagunda: Your Ideal Rental Apartment Destination</h1>
+      <p>Finding the perfect rental apartment can be a daunting task, but Wagunda stands out as a haven for those seeking comfort, convenience, and community. Located in the heart of a vibrant neighborhood, Wagunda rental apartments offer a unique blend of modern amenities and serene living. Whether you’re a young professional, a growing family, or someone looking to downsize, Wagunda has something special to offer..</p>
+      <Link to="/contact">Contact Us</Link>
+    </div>
+  );
+}
+
+export default Home;
+
+
